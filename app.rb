1d4096bd2c61d5b53bf2afff991ab5f40b99744a
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-26 19:57:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,4 +22,14 @@ get '/:operation/:number1/:number2'do
   @number1 = params[:number1].to_i
   @number2 = params [:number2].to_i
   
-  if @operation == "add"ss
+  if @operation == "add"
+    @number1 + @number2
+  elsif @operation == "subtract"
+    @number1- @number2
+  elsif @operation == "multiply"
+    @number1 * @number2 
+  else
+    @number1 / @number2
+  end
+end
+   
